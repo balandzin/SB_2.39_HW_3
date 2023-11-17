183c215aa227615dd1fc566ae43eb0c92e358612
balandzin <balandzin2602@gmail.com>
2023-11-17 15:50:10 +0300

    init commit

---
+++ HW_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_3
+//
+//  Created by Антон Баландин on 17.11.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
