9fcfdb42fc727aa949cac0cb12a033cc04048f28
balandzin <balandzin2602@gmail.com>
2023-11-17 23:34:13 +0300

    finish hw_3

--- HW_3/ViewController.swift
+++ HW_3/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorfulLabel: UIView!
+
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    
+    
+    @IBOutlet var redValueLabel: UILabel!
+    @IBOutlet var greenValueLabel: UILabel!
+    @IBOutlet var blueValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorfulLabel.layer.cornerRadius = 25
+        
+        changeColor()
     }
 
-
+    @IBAction func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        changeColor()
+    }
+    
+    private func changeColor() {
+        colorfulLabel.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
 
